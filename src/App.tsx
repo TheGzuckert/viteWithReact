2073abc9d68f0ed7769a0f19c00dfaf41fbdfa15
TheGzuckert <games.zarck@gmail.com>
2023-05-28 00:40:47 -0300

    studdy react with TS

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Post, PostType } from "./components/Post"
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import './global.css'
+import styles from './App.module.css'
+
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/TheGzuckert.png',
+      name: 'Gustavo Zuckert',
+      role: 'Análista de suporte @Bten'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-26 12:42:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/WeDias.png',
+      name: 'Wesley Dias',
+      role: 'Software Engineer @isaac'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-26 13:14:00')
+  },
+  
+]
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return(
+              <Post
+                key={post.id}
+                post={post}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
